03f952c812862e7bee2b67153fc8fe076e575a1c
michellenikeetha <88478424+michellenikeetha@users.noreply.github.com>
2023-02-16 14:19:22 +0530

    Delete Stadia directory

--- Stadia/linkDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Open a new connection to the MySQL server
-$linkDB = mysqli_connect("localhost","root","","stadia-new");  
-  if (mysqli_connect_error()){ //for connection error finding
-  die ('There was an error while connecting to database');
-  }
-    ?>
