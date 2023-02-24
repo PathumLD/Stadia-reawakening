fc1e63611d1ad204effaec2d210b83f9b5cf66c4
michellenikeetha <88478424+michellenikeetha@users.noreply.github.com>
2023-02-24 11:58:01 +0530

    Delete Stadia/Coach directory

--- Stadia/Coach/inc/styles.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="css/sidebar.css">
-<link rel="stylesheet" href="css/layout.css">
-<link rel="stylesheet" href="css/navbar.css">
-<link rel="stylesheet" href="css/table.css">
-<link rel="stylesheet" href="css/login.css">
