43549e73c9948800382fb52de7bd41083be33d82
michellenikeetha <88478424+michellenikeetha@users.noreply.github.com>
2023-04-17 19:35:26 +0530

    Add files via upload

---
+++ client/clientslotsbadminton1.php
@@ -0,0 +1,199 @@
+<?php session_start();?>
+<?php include("../linkDB.php"); //database connection function ?> 
+
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title> Stadia </title>
+    
+    <!-- Fontawesome CDN Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/client.css">
+ 
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php include('../include/javascript.php'); ?>
+    <?php include('../include/styles.php'); ?>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" /> -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+
+    <script>
+   
+  $(document).ready(function() {
+   var calendar = $('#calendar').fullCalendar({
+    editable:true,
+    header:{
+     left:'prev,next today',
+     center:'title',
+     right:'month,agendaWeek,agendaDay'
+    },
+    events: 'slotsbadminton1load.php',
+    selectable:true,
+    selectHelper:true,
+    select: function(start, end, allDay)
+    {
+     var title = prompt("Enter Event Title");
+     if(title)
+     {
+      var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+      var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+      $.ajax({
+       url:"slotsbadminton1insert.php",
+       type:"POST",
+       data:{title:title, start:start, end:end},
+       success:function()
+       {
+        calendar.fullCalendar('refetchEvents');
+        alert("Added Successfully");
+       }
+      })
+     }
+    },
+    editable:true,
+    eventResize:function(event)
+    {
+     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+     var title = event.title;
+     var id = event.id;
+     $.ajax({
+      url:"slotsbadminton1update.php",
+      type:"POST",
+      data:{title:title, start:start, end:end, id:id},
+      success:function(){
+       calendar.fullCalendar('refetchEvents');
+       alert('Event Update');
+      }
+     })
+    },
+
+    eventDrop:function(event)
+    {
+     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+     var title = event.title;
+     var id = event.id;
+     $.ajax({
+      url:"slotsbadminton1update.php",
+      type:"POST",
+      data:{title:title, start:start, end:end, id:id},
+      success:function()
+      {
+       calendar.fullCalendar('refetchEvents');
+       alert("Event Updated");
+      }
+     });
+    },
+
+    eventClick:function(event)
+    {
+     if(confirm("Are you sure you want to remove it?"))
+     {
+      var id = event.id;
+      $.ajax({
+       url:"slotsbadminton1delete.php",
+       type:"POST",
+       data:{id:id},
+       success:function()
+       {
+        calendar.fullCalendar('refetchEvents');
+        alert("Event Removed");
+       }
+      })
+     }
+    },
+
+   });
+  });
+   
+  </script>
+
+   </head>
+<body onload="initClock()">
+
+<div class="sidebar">
+
+    <?php include('../include/clientsidebar.php'); ?>
+
+</div>
+
+<section class="home-section">
+
+    <nav>
+
+        <?php include('../include/navbar.php'); ?>
+
+    </nav>
+
+    <div class="home-content">
+
+        <div class="main-content">
+
+            <?php $var = $_SESSION['email']; ?>
+
+            <h1>Slots - Badminton Court 1</h1>
+
+            <div class="content">
+
+              <div class="segmented-control">
+        
+                <input type="radio" name="radio2" value="1" id="tab-1" checked/>
+                <label for="tab-1" class= "segmented-control__1">
+                  <p><a href="clientslotsbadminton1.php">Court 1</a></p>
+                </label>
+                                
+                <input type="radio" name="radio2" value="2" id="tab-2" />
+                <label for="tab-2" class= "segmented-control__2">
+                  <p><a href="clientslotsbadminton2.php">Court 2</a></p>
+                </label>
+                
+                <div class="segmented-control__color"></div>
+              </div>
+
+            <div class="container">
+              <div id="calendar"></div>
+            </div>
+
+          </div>
+
+        </div>
+
+    </div>
+
+    <footer>
+        <div class="foot">
+          <?php include("../include/footer.php"); ?>
+        </div>
+    </footer> 
+
+</section>
+
+</body>
+</html>
+
+<script>
+        /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+        var dropdown = document.getElementsByClassName("dropdown-btn");
+        var i;
+        
+        for (i = 0; i < dropdown.length; i++) {
+          dropdown[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+              dropdownContent.style.display = "none";
+            } else {
+              dropdownContent.style.display = "block";
+            }
+          });
+        }
+</script>
