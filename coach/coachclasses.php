1c97887e1f5aa2eb6940d45947e3e03e408509cc
PathumLD <89245419+PathumLD@users.noreply.github.com>
2023-05-02 16:39:26 +0530

    Add files via upload

--- coach/coachclasses.php
+++ coach/coachclasses.php
@@ -12,14 +12,12 @@
     <!-- Fontawesome CDN Link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../css/coach.css">
+    <link rel="stylesheet" href="../css/coach/coachclasses.css">
  
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
      <?php include('../include/javascript.php'); ?>
      <?php include('../include/styles.php'); ?>
-
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
    </head>
 <body onload="initClock()">
@@ -42,112 +40,161 @@
 
         <div class="main-content">
 
-        <?php $var = $_SESSION['email']; ?>
+          <?php $var = $_SESSION['email']; ?>
 
-            <h1>My Classes</h1>
+                <h1>My Classes</h1>
 
+            <div class="content">
 
-            <table class = "searchtable">
-            <tr><td><a href="coachaddnewclass.php"><i class="fa fa-plus-circle" id="plus" style="font-size:36px;"></i></a></td>
+                <table class = "searchtable">
+                    <tr><td><a href="coachaddnewclass.php" class = "text"><i class="fa fa-plus-circle" id="plus" style="font-size:36px;"></i>        Add Class</a></td>
 
-            <td><form method="POST">
+                        <td>
+                            <form method="POST">
+                    
+                                <select class="search"name="search">
+                                <option value="all">All</option>
+                                <option value="monday">Monday</option>
+                                <option value="tuesday">Tuesday</option>
+                                <option value="wednesday">Wednesday</option>
+                                <option value="thursday">Thursday</option>
+                                <option value="friday">Friday</option>
+                                <option value="saturday">Saturday</option>
+                                <option value="sunday">Sunday</option>
+                                </select>
+                                <input type="submit" name="go" value="Search" id="searchbtn">
+                                <a href="coachclasses.php"><input type="submit" name="reset" value="reset" id = "resetbtn"></a>
+                        
+                            </form>
+                        </td>
+                    </tr>
+                </table>
             
-                    <select class="search"name="search">
-                    <option value="all">All</option>
-                    <option value="monday">Monday</option>
-                    <option value="tuesday">Tuesday</option>
-                    <option value="wednesday">Wednesday</option>
-                    <option value="thursday">Thursday</option>
-                    <option value="friday">Friday</option>
-                    <option value="saturday">Saturday</option>
-                    <option value="sunday">Sunday</option>
-                    </select>
-                    <input type="submit" name="go" value="Search" id="searchbtn">
-                    <a href="coachclasses.php"><input type="submit" name="reset" value="reset" id = "resetbtn"></a>
-                
-                </form></td></tr>
-        </table>
+                <div class="activeclasses">
+                    
+                    <h3> Active Classes </h3>
 
-            
-            
-            <table class="table">
+                    <div class = "datatable">
 
-                <tr>
+                        <table class="table">
 
-                    <th> Day </th>
-                    <th> Sport </th>
-                    <th> Time</th>
-                    <th> Age Group </th>
-                    <th> No. of Students </th>
-                    <th> Student Details </th>
-                    <th> Action </th>
+                            <tr>
 
-                </tr>
+                                <th> Day </th>
+                                <th> Sport </th>
+                                <th> Time</th>
+                                <th> Age Group </th>
+                                <th> No. of Students </th>
+                                <th> Student Details </th>
+                                <th> Action </th>
 
-                            <?php
+                            </tr>
 
-                                if(isset($_POST['go'])) {
-                                    $search = $_POST['search'];
-                                } else {
-                                    $search = null;
-                                }
+                                        <?php
 
-                                if($search == 'all') {
-                                    $query = "SELECT * FROM coach_classes";
-                                } else {
-                                    $query = "SELECT * FROM coach_classes WHERE date LIKE '%$search%' && status = '1'";
-                                }
+                                            if(isset($_POST['go'])) {
+                                                $search = $_POST['search'];
+                                            } else {
+                                                $search = null;
+                                            }
 
-                                $res = mysqli_query($linkDB, $query);
+                                            if($search == 'all') {
+                                                $query = "SELECT * FROM coach_classes";
+                                            } else {
+                                                $query = "SELECT * FROM coach_classes WHERE date LIKE '%$search%' && status = '1'";
+                                            }
 
-                                if($res == TRUE) {
-                                    $count = mysqli_num_rows($res);
+                                            $res = mysqli_query($linkDB, $query);
 
-                                    if($count > 0) {
-                                        while($rows = mysqli_fetch_assoc($res)) {
-                                            $id = $rows['id'];
+                                            if($res == TRUE) {
+                                                $count = mysqli_num_rows($res);
 
-                                            echo "<tr id='row_$id'>  
-                                                <td>" . $rows["date"]. "</td>
-                                                <td>" . $rows["sport"]. "</td>
-                                                <td>" . $rows["time"]. "</td>
-                                                <td>" . $rows["age_group"]. "</td>
-                                                <td>" . $rows["no_of_students"]. "</td>
-                                                <td><a href='coachstudentdetails.php?id=$id;'>View</a> </td>
-                                                <td><a href='coachupdateclass.php?id=$id;'><i class='fa fa-edit' id='edit' style='font-size:24px'></i></a>
-                                                
-                                            </tr>";
-                                        }
-                                    } else {
-                                        echo "0 results";
-                                    }
-                                }
-                                
-                                ?>
+                                                if($count > 0) {
+                                                    while($rows = mysqli_fetch_assoc($res)) {
+                                                        $id = $rows['id'];
 
+                                                        echo "<tr id='row_$id'>  
+                                                            <td>" . $rows["date"]. "</td>
+                                                            <td>" . $rows["sport"]. "</td>
+                                                            <td>" . $rows["time"]. "</td>
+                                                            <td>" . $rows["age_group"]. "</td>
+                                                            <td>" . $rows["no_of_students"]. "</td>
+                                                            <td><a href='coachstudentdetails.php?id=$id;'>View</a> </td>
+                                                            <td><a href='coachupdateclass.php?id=$id;'><i class='fa fa-edit' id='edit' style='font-size:24px'></i></a>
+                                                            
+                                                        </tr>";
+                                                    }
+                                                } else {
+                                                    echo "0 results";
+                                                }
+                                            }
+                                            
+                                        ?>
 
+                        </table>
+                    
+                    </div>
 
+                </div>
 
+                <div class="pendingclasses">
 
-     
-            </table>
+                    <h3> Pending Classes </h3>
 
-            <table class ="table2">
-                <tr>
-                    <th>Pending Classes</t>
-                </tr>
-            </table>
+                    <div class = "frame2">
+
+                        <table class="table2">
+                            <tr>
+                                <th> Name </th>
+                                <th> Sport </th>
+                                <th> Time </th>
+                            </tr>
+                            <tr>
+                                <td> John Doe </td>
+                                <td> Basketball </td>
+                                <td> 10:00 </td>
+                            </tr>
+                            <tr>
+                                <td> Jane Doe </td>
+                                <td> Volleyball </td>
+                                <td> 11:00 </td>
+                            </tr>
+                            <tr>
+                                <td> Jane Doe </td>
+                                <td> Volleyball </td>
+                                <td> 11:00 </td>
+                            </tr>
+                            <tr>
+                                <td> Jane Doe </td>
+                                <td> Volleyball </td>
+                                <td> 11:00 </td>
+                            </tr>
+                            <tr>
+                                <td> Jane Doe </td>
+                                <td> Volleyball </td>
+                                <td> 11:00 </td>
+                            </tr>
+                        </table>
+
+                    </div>
+
+                </div>
+                    
+                <div class="paidclasses">
+
+                    <h3> Paid </h3>
+
+                </div>
+
+            </div>
 
         </div>
-
-    </div>
-    </div>
 
     </div>
 
     <footer>
         <div class="foot">
-            <span>Created By <a href="#">Stadia.</a> | &#169; 2023 All Rights Reserved</span>
+          <?php include("../include/footer.php"); ?>
         </div>
     </footer> 
 
