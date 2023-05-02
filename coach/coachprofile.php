a7bdcd5c4e894dbe385d22cea7b78faf105b7f78
PathumLD <89245419+PathumLD@users.noreply.github.com>
2023-05-02 18:09:06 +0530

    Add files via upload

--- coach/coachprofile.php
+++ coach/coachprofile.php
@@ -46,29 +46,106 @@
 
           <div class="content">
 
-                <div class="profilepic">
+                <div class="profiledata">
                           
-                          
+                <table id="tableprofile">   
+
+                    <?php
+
+                          $sql = "SELECT * FROM users WHERE email = '".$var."'";
+
+                          $result = $linkDB->query($sql);
+
+                          if ($result-> num_rows>0){
+                              while($row = $result->fetch_assoc()){
+
+                                  echo "<tr>
+                                      <td class='mylabel'>Name:</td>
+                                      <td class='mydata'>".$row['fname']." ".$row['lname']."</td>
+                                      </tr>
+
+                                      <td class='mylabel'>Gender:</td>
+                                      <td class='mydata'>".$row['gender']."</td>
+                                      </tr>
+
+                                      <td class='mylabel'>Phone:</td>
+                                      <td class='mydata'>".$row['phone']."</td>
+                                      <td><button class='open-button' onclick='openForm()'><i class='fa fa-pencil-square-o' ></i></button>
+                                        <div class='form-popup' id='myForm'>
+                                          <form class='form-container' action='' method='POST'>
+                                        
+                                            <label for='phone'><b>Update Phone</b></label>
+                                            <input type='tel' placeholder='Enter phone' name='phone' pattern='[0-9]{10}'required>
+                                        
+                                            <input type='submit' class='btn' id='update-btn' name='update' value='Update'>
+                                            <button type='button' class='cancelbtn' onclick='closeForm()'><i class='fa fa-times' ></i></button>
+                                          </form>
+                                        </div></td>
+                                      </tr>
+
+                                      <td class='mylabel'>Date of Birth:</td>
+                                      <td class='mydata'>".$row['dob']."</td>
+                                      </tr>
+                                      <td class='mylabel'>NIC / Guardian NIC:</td>
+                                      <td class='mydata'>".$row['NIC']."</td>
+                                      </tr>
+
+                                      <td class='mylabel'>Emergency Contact Number:</td>
+                                      <td class='mydata'>".$row['emphone']."</td>
+                                      <td><button class='open-button' onclick='openForm2()'><i class='fa fa-pencil-square-o' ></i></button>
+                                        <div class='form-popup' id='myForm2'>
+                                          <form class='form-container' action='' method='POST'>
+                                        
+                                            <label for='emphone'><b>Emergency Contact Number</b></label>
+                                            <input type='tel' placeholder='Enter number' name='emphone' pattern='[0-9]{10}'required>
+                                        
+                                            <input type='submit' class='btn' id='update-btn' name='update2' value='update'>
+                                            <button type='button' class='cancelbtn' onclick='closeForm2()'><i class='fa fa-times' ></i></button>
+                                          </form>
+                                        </div></td>
+                                      </tr>
+                                      
+                                      <td class='mylabel'>Emergency Contact Name:</td>
+                                      <td class='mydata'>".$row['emname']."</td>
+                                      <td><button class='open-button' onclick='openForm3()'><i class='fa fa-pencil-square-o' ></i></button>
+                                        <div class='form-popup' id='myForm3'>
+                                          <form class='form-container' action='' method='POST'>
+                                        
+                                            <label for='emname'><b>Emergency Contact Name</b></label>
+                                            <input type='text' placeholder='Enter name' name='emname' required>
+                                        
+                                            <input type='submit' class='btn' id='update-btn' name='update3' value='update'>
+                                            <button type='button' class='cancelbtn' onclick='closeForm3()'><i class='fa fa-times' ></i></button>
+                                          </form>
+                                        </div></td>
+                                      </tr>";
+                                  
+                              }
+                          }
+
+                    ?>
+
+                </table>     
 
                           
                           
                 </div>
 
-                <div class="profiledetails">
+                <div class="cv">
 
-                          
+                
+
+                    <div class="button">
+                        <a href="coachchangepassword.php"> Change Password </a>
+                        <a href="coachupdateprofilephoto.php">Update Profile Photo</a>
+                        <a href="coachuploadcv.php">Upload CV</a>
+                    </div>    
 
                         
 
                 </div>
 
           </div>
-
-                  <div class="button">
-                    <a href="coachchangepassword.php"> Change Password </a>
-                    <a href="coachupdateprofilephoto.php">Update Profile Photo</a>
-                    <a href="coachuploadcv.php">Upload CV</a>
-                  </div>
 
         </div>
 
