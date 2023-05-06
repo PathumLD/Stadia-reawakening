8e331e85b43652c4b184ddc64d8c115de0c4b6b4
fiya99banu <92871864+fiya99banu@users.noreply.github.com>
2023-05-06 10:50:40 +0530

    Add files via upload

---
+++ include/suppliersidebar.php
@@ -0,0 +1,13 @@
+<div class="logo">
+        <span class="logo-img"><img src="../images/logo.png" alt="logo"></span>
+    </div>
+
+    <div class="nav-links">
+        <a href="suppliermyorders.php"><i class="fa fa-list-ul"></i><span class="links_name">View My Orders</span></a>
+        <a href="supplierpayments.php"><i class="fa fa-clone"></i><span class="links_name">Payments</span></a>
+        <a href="suppliersupplyorders.php"><i class="fa fa-users"></i><span class="links_name">Supply Orders</span></a>
+        
+        <div class="log-out">
+            <a href="logout.php"><i class="fa fa-sign-out" ></i><span class="links_name">Log out</span></a>
+        </div>
+    </div>
